e0d193f21862f7cb09ddb9152f2058b12461d4cf
drsmile <drsmile.1001@gmail.com>
2020-02-14 13:23:15 +0800

    自動啟動瀏覽器

--- StaticFilesWebServer/Startup.cs
+++ StaticFilesWebServer/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,9 +16,14 @@ namespace StaticFilesWebServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration configuration)
         {
-            var baseUrl = configuration.GetValue<string>("Kestrel:Endpoints:Http:Url");
             app.UseStaticFiles();
-            System.Diagnostics.Process.Start("cmd", $"/C start {baseUrl}/index.html");
+            var baseUrl = configuration.GetValue<string>("Kestrel:Endpoints:Http:Url");
+            var autoStartBrowserPath = configuration.GetValue<string>("AutoStartBrowserPath");
+            if (!string.IsNullOrWhiteSpace(autoStartBrowserPath))
+            {
+                System.Diagnostics.Process.Start("cmd", $"/C start {baseUrl}{autoStartBrowserPath}");
+                System.Console.WriteLine($"首頁路徑: {baseUrl}{autoStartBrowserPath}");
+            }
         }
     }
 }
