1533fea192aba4dcbfbdf78a89ba74d32f69c1d0
drsmile <drsmile.1001@gmail.com>
2020-02-14 13:11:16 +0800

    加入專案檔案。

---
+++ StaticFilesWebServer/Startup.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StaticFilesWebServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration configuration)
+        {
+            var baseUrl = configuration.GetValue<string>("Kestrel:Endpoints:Http:Url");
+            app.UseStaticFiles();
+            System.Diagnostics.Process.Start("cmd", $"/C start {baseUrl}/index.html");
+        }
+    }
+}
